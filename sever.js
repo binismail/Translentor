ddec1e9f370e30bbf6228a43409527c7c66cb992
khalid ismail <kabcoder@gmail.com>
2020-08-09 10:50:41 -0700

    pusher setup

--- sever.js
+++ sever.js
@@ -4,14 +4,18 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=> console.log(`server running in ${PORT}`))
+app.listen(PORT, ()=>{
+    const puhser = new Pusher({
+        app_id : "1052208",
+        key : "cde8f3b357ee1e82b96a",
+        secret: "05ca0836f128112d50a8",
+        cluster : "eu"
+    })
 
-
-const puhser = new Pusher({
-    app_id : "1052208",
-    key : "cde8f3b357ee1e82b96a",
-    secret: "05ca0836f128112d50a8",
-    cluster : "eu"
+    Pusher.subscribe('channel-name')
 })
+
+
+
 
 Pusher.trigger('my-channel', 'my-event', {'message' : 'hello am a new channel'})
